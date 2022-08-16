b93913761881627c78c4010acc7e2267eb4962ef
Junhyeok <ojh1489@gmail.com>
2022-08-16 21:48:06 +0900

    관리자 기능 작업중

--- hallo03talk/WebContent/index.jsp
+++ hallo03talk/WebContent/index.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+    <% String contextPath =request.getContextPath(); %>
 <!DOCTYPE html>
 <html>
   <head>
@@ -184,7 +185,7 @@
     <header>
       <nav>
         <ul class="menu1">
-          <li><a href="">공지</a></li>
+          <li><a href="">공지사항</a></li>
           <li><a href="">로그인</a></li>
            <li><a href="">회원가입</a></li>
         </ul>
---
+++ hallo03talk/src/com/h3/admin/controller/AdminLoginController.java
@@ -0,0 +1,47 @@
+package com.h3.admin.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.h3.admin.service.AdminService;
+import com.h3.admin.vo.AdminVo;
+@WebServlet(urlPatterns = "/admin/login")
+public class AdminLoginController extends HttpServlet {
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String adminId = req.getParameter("adminId");
+		String adminPwd = req.getParameter("adminPwd");
+		
+		//데이터 뭉치기
+		AdminVo vo = new AdminVo();
+		vo.setId(adminId);
+		vo.setPwd(adminPwd);
+		
+		//서비스 로직 실행
+	AdminVo loginAdmin = new AdminService().login(vo);
+	
+	//결과에 따라 화면 선택
+	if (loginAdmin != null) {
+		//로그인 성공 // 세션에 로그인 유저정보 담기
+		req.getSession().setAttribute("loginMember", loginAdmin);	
+		req.getSession().setAttribute("alertMsg", "관리자 로그인 성공");
+		
+//		req.getRequestDispatcher("/semi").forward(req, resp);
+		resp.sendRedirect("/semi");
+		
+	}else{
+		//로그인실패
+		req.setAttribute("errorMsg", "로그인실패!");
+		req.getRequestDispatcher("/views/error/errorPage.jsp").forward(req, resp);
+	}
+	}
+	}
+	
+
+
