9ce613bff14285bd13cc4310159041a2f5625011
DESKTOP-JNI98I1\chan <chanrb0966@gmail.com>
2022-08-21 06:31:34 +0900

    Revert "Revert "Revert "섬머노트 작업"""
    This reverts commit 072cb42dfd7c98f63b334a527407c9641f7983c8.

--- hallo03talk/src/com/h3/with/controller/WithImgUploadController.java
+++ hallo03talk/src/com/h3/with/controller/WithImgUploadController.java
@@ -8,20 +8,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.h3.with.service.WithImgUploadService;
-
 
 @WebServlet(urlPatterns = "/with/imgUpload")
 public class WithImgUploadController extends HttpServlet{
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		req.setCharacterEncoding("UTF-8");
-		resp.setCharacterEncoding("UTF-8");
 		
-		String base64Img = req.getParameter("img");
-		String realPath = req.getServletContext().getRealPath("/resources/upload/with/");
-		String data = new WithImgUploadService().saveImgReturnUrl(base64Img,realPath);
-		
-		resp.getWriter().write(data);
+		String img = req.getParameter("img");
+		System.out.println(img);
 	}
 }
