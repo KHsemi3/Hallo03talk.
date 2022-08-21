90a7582cc051ea08b4a8f7ccd9b55c8eb083765f
Junhyeok <ojh1489@gmail.com>
2022-08-21 13:21:43 +0900

    뭐야 이거

--- hallo03talk/src/com/h3/admin/controller/AdminReportReplyController.java
+++
@@ -1,8 +0,0 @@
-package com.h3.admin.controller;
-
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-@WebServlet(urlPatterns = "/admin/reportReply")
-public class AdminReportReplyController extends HttpServlet {
-
-}
